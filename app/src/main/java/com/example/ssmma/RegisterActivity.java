8fb7c4679b4f2b3caef7752d8d2fb475bb9fb8f5
Pawel Rus <pawelrus.637@gmail.com>
2024-04-01 22:42:35 +0200

    register window with all necessary data

--- app/src/main/java/com/example/ssmma/RegisterActivity.java
+++ app/src/main/java/com/example/ssmma/RegisterActivity.java
@@ -9,16 +9,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private EditText email, password, name;
+    private Button mRegister;
+    private TextView existaccount;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        email = findViewById(R.id.register_email);
+        name = findViewById(R.id.register_name);
+        password = findViewById(R.id.register_password);
+        mRegister = findViewById(R.id.register_button);
+        existaccount = findViewById(R.id.homepage);
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Register");
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emaill = email.getText().toString().trim();
+                String uname = name.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(emaill).matches()) {
+                    email.setError("Invalid Email");
+                    email.setFocusable(true);
+                } else if (pass.length() < 6) {
+                    password.setError("Length Must be greater than 6 character");
+                    password.setFocusable(true);
+                } else {
+                    registerUser(emaill, pass, uname);
+                }
+            }
+        });
+        existaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
         });
     }
 }
